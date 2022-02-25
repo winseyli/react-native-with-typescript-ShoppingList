77ec050614a9f5bd0c86f514e1e65c1f14ea8a8c
Winsey Li <winsey.li@gmail.com>
2022-02-25 15:35:56 +1300

    add "AddItem"

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Header from './src/components/Header';
+import AddItem, {IItem} from './src/components/AddItem'; /* import AddItem and interface*/
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Shopping List" />
+      <View style={styles.contentWrapper}>
+        <AddItem />
+      </View>
     </SafeAreaView>
   );
 };
@@ -14,6 +18,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#e8e7e3',
+  },
+  contentWrapper: {
+    padding: 20,
   },
 });
 
